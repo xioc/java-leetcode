19c40a7ca0efff9ce32025045c6d89f1e34f4999
wanter <hyldev@qq.com>
2020-09-09 21:23:42 +0800

    增加剑指offer 43题

--- src/com/github/xioc/lcof/Jz34.java
+++ src/com/github/xioc/lcof/Jz34.java
@@ -1,8 +1,38 @@
 package com.github.xioc.lcof;
 
+import com.github.xioc.basic.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author huyanglin
  * @date 2020/9/9 11:28
  */
 public class Jz34 {
+    LinkedList<Integer> path = new LinkedList<>();
+    private LinkedList<List<Integer>> res = new LinkedList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        recur(root, sum);
+        return res;
+    }
+
+    private void recur(TreeNode root, int tar) {
+        if (root == null) {
+            return;
+        }
+
+        path.add(root.val);
+        tar = tar - root.val;
+        if (tar == 0 && root.left == null && root.right == null) {
+            res.add(new LinkedList<>(path));
+        }
+        path.removeLast();
+        recur(root.left, tar);
+        recur(root.right, tar);
+    }
+
 }
