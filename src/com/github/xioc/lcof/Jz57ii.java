289cb4fe51351783d4f9bcc3611f6acc6f3a9284
hyldev <hyldev@qq.com>
2020-09-16 09:56:20 +0800

    增加剑指offer 57-2

--- src/com/github/xioc/lcof/Jz57ii.java
+++ src/com/github/xioc/lcof/Jz57ii.java
@@ -1,12 +1,58 @@
 package com.github.xioc.lcof;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author hyldev
  * @date 2020/9/15 20:53
+ *
+ * 思路：双指针
+ * 掌握程度：看题解思路，自己代码实现
+ * note：
  */
 public class Jz57ii {
+    List<int[]> res = new ArrayList<>();
+
     public int[][] findContinuousSequence(int target) {
-        
-        return null;
+
+        int start = 1, end = 2;
+        int sum = 0;
+        while (start < end) {
+            sum = ((end + start) * (end - start + 1)) >> 1;
+            if (sum < target) {
+                end++;
+            } else if (sum > target) {
+                start++;
+            } else {
+                res.add(generateList(start, end));
+                start++;
+            }
+
+        }
+        return toArray(res);
+    }
+
+    private int[][] toArray(List<int[]> data) {
+        int[][] res = new int[data.size()][];
+        for (int i = 0; i < data.size(); i++) {
+            res[i] = data.get(i);
+        }
+        return res;
+    }
+
+
+    private int[] generateList(int start, int end) {
+        int[] tmp = new int[end - start + 1];
+        int index = 0;
+        for (int i = start; i <= end; i++) {
+            tmp[index] = i;
+            index++;
+        }
+        return tmp;
+    }
+
+    public static void main(String[] args) {
+        new Jz57ii().findContinuousSequence(25);
     }
 }
